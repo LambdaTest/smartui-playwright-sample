d52308a4a3dc5738d4b55c755dd79fced2561ff7
Jeevesh <109514265+JeeveshJ7@users.noreply.github.com>
2023-12-11 11:31:51 +0530

    Create playwright-smartui.js

---
+++ playwright-smartui.js
@@ -0,0 +1,70 @@
+const { chromium } = require('playwright')
+const { expect } = require('@playwright/test');
+
+(async () => {
+  const capabilities = {
+    'browserName': 'Chrome', // Browsers allowed: `Chrome`, `MicrosoftEdge`, `pw-chromium`, `pw-firefox` and `pw-webkit`
+    'browserVersion': 'latest',
+    'LT:Options': {
+      'platform': 'Windows 10',
+      'build': 'Playwright SmartUI Build',
+      'name': 'Playwright SmartUI Test',
+      'user': process.env.LT_USERNAME || '<USERNAME>',
+      'accessKey': process.env.LT_ACCESS_KEY || '<ACCESS KEY>',
+      'network': true,
+      'video': true,
+      'console': true,
+      'smartUIProjectName': 'Playwright-SmartUI-Project4',
+      // 'smartUIBaseline': false
+    }
+  }
+
+  const githubURL = process.env.GITHUB_URL
+  if(githubURL){
+    capabilities['LT:Options']['github'] = {
+      url : githubURL
+    }
+  }
+  
+  const browser = await chromium.connect({
+    wsEndpoint: `wss://cdp.lambdatest.com/playwright?capabilities=${encodeURIComponent(JSON.stringify(capabilities))}`
+  })
+
+  const page = await browser.newPage()
+
+  await page.goto('https://www.lambdatest.com')
+
+  // Add the following command in order to take screenshot in SmartUI
+  await page.evaluate((_) => {},
+    `lambdatest_action: ${JSON.stringify({ action: 'smartui.takeScreenshot', arguments: { fullPage: true, screenshotName: 'search-lambdatest' }
+    })}`) // Add a relevant screenshot name here
+
+  const element = await page.$('[id="sb_form_q"]')
+  // await element.click()
+  await element.type('LambdaTest')
+  await page.waitForTimeout(1000)
+  await page.keyboard.press('Enter')
+  await page.waitForSelector('[class=" b_active"]')
+  const title = await page.title()
+
+  try {
+    expect(title).toEqual('LambdaTest - Search')
+    // Mark the test as completed or failed
+    await page.evaluate(_ => {}, `lambdatest_action: ${JSON.stringify({ action: 'setTestStatus', arguments: { status: 'passed', remark: 'Title matched' } })}`)
+  } catch {
+    await page.evaluate(_ => {}, `lambdatest_action: ${JSON.stringify({ action: 'setTestStatus', arguments: { status: 'failed', remark: 'Title not matched' } })}`)
+  }
+
+  await page.goto("https://www.lambdatest.com")
+
+  await page.evaluate((_) => {},
+    `lambdatest_action: ${JSON.stringify({ action: 'smartui.takeScreenshot', arguments: { fullPage: true, screenshotName: 'lambdatest-website' }
+    })}`) 
+    await page.evaluate((_) => {},`lambdatest_action: ${JSON.stringify({ action: 'smartui.takeScreenshot', arguments: { fullPage: false, screenshotName: '<Screenshot Name>'}})}`)
+  await page.goto("https://www.lambdatest.com/support/api-doc/")
+  await page.evaluate((_) => {},
+    `lambdatest_action: ${JSON.stringify({ action: 'smartui.takeScreenshot', arguments: { fullPage: true, screenshotName: 'api-doc' }
+    })}`) 
+
+    await browser.close()
+})()
